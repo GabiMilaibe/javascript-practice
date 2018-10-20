7d6199ca702cc1867ecc8fa1f3f4b5386114e677
Gabriela <gabriela.milaibe@gmail.com>
2018-10-21 00:37:45 +1100

    Add blackjack

---
+++ ArrayToString.js
@@ -0,0 +1,14 @@
+
+const array = [1,2,3,4,5];
+// console.log(array[0]);
+
+const arrayString = [];
+
+for (let i = 0; i < array.length; i++){
+  arrayString.push(array[i].toString());
+//console.log(array[i].toString());
+//console.log(array[i]);
+};
+console.log(arrayString);
+
+ 
